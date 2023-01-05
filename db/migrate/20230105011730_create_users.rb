6c4ca25ca06759b0c571bab501a4a911c5596807
Won Rhim <wwrk22@gmail.com>
2023-01-04 20:18:12 -0500

    Add User model and migrate to DB

---
+++ db/migrate/20230105011730_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.text :username
+      t.text :email
+      t.text :password
+
+      t.timestamps
+    end
+  end
+end
